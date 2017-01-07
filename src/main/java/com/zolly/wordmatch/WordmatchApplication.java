4e12f80254627d21b00de3db7692bd0efdf36b1d
Oliver Palmer <ollypalmer1@gmail.com>
2017-01-07 21:50:05 +0000

    initial commit

---
+++ src/main/java/com/zolly/wordmatch/WordmatchApplication.java
@@ -0,0 +1,12 @@
+package com.zolly.wordmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordmatchApplication.class, args);
+	}
+}
